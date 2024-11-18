34fa72b0c4faa6b1ebd44db3aaca9c3cf359ebde
“ConsolataMutheu” <consolata.mbithi@aws.ajiraemobilis.org>
2024-11-18 12:36:14 +0300

    modified

--- blog/models.py
+++ blog/models.py
@@ -1,18 +1,23 @@
 from django.db import models
 from django.utils import timezone
+from django.conf import settings
 
 # Create your models here.
+from django.db import models
+from django.utils import timezone
+
 class Post(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     text = models.TextField()
-    created_date = models.DateTimeField(auto_now_add=True)
+    created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
-  def publish(self):
-      self.published_date = timezone.now()
-      self.save()
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()  # Save the object after publishing
 
-      def __str__(self):
-          return self.title
+    def __str__(self):
+        return self.title  # String representation for the object, typically used in the admin
+
 
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse("Welcome to blog.")
+def post_list(request):
+    return render(request,'post_list.html')
